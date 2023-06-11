1f5bfd9c7788d66587dd52bd1695dc99e8578669
Jason Lee <hylee.jason@gmail.com>
2023-06-11 12:10:09 +0800

    Make calc work

--- calculator.js
+++ calculator.js
@@ -1,28 +1,92 @@
-// Get the calculator screen elements
-const storedInput = document.getElementById("stored-input");
-const result = document.getElementById("result");
+// Operator functions
 
-// Get all the calculator buttons
-const buttons = document.querySelectorAll(".button");
+function add(a, b) {
+  return a + b;
+}
 
-// Add event listeners to the calculator buttons
-buttons.forEach((button) => {
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(num1, num2, operator) {
+  switch(operator) {
+      case "+":
+      return add(num1, num2);
+      case "-":
+      return subtract(num1, num2);
+      case "*":
+      return multiply(num1, num2);
+      case "/":
+      return divide(num1, num2);
+      default:
+      return null;
+  }
+}
+
+let displayValue = "";
+
+document.querySelectorAll(".number").forEach(button => {
   button.addEventListener("click", () => {
-    const buttonValue = button.dataset.value;
-
-    if (buttonValue === "clear") {
-      storedInput.textContent = "";
-      result.textContent = "";
-    } else if (buttonValue === "delete") {
-      storedInput.textContent = storedInput.textContent.slice(0, -1);
-    } else if (buttonValue === "=") {
-      const equation = storedInput.textContent;
-      if (equation) {
-        const answer = eval(equation);
-        result.textContent = answer;
+      if (displayValue === "0") {
+          displayValue = button.textContent;
+      } else if (displayValue === "-0") {
+          displayValue = "-" + button.textContent;
+      } else {
+          displayValue += button.textContent;
       }
-    } else {
-      storedInput.textContent += buttonValue;
-    }
+      document.getElementById("display").textContent = displayValue;
   });
+});
+
+document.querySelectorAll(".decimal").forEach(button => {
+  button.addEventListener("click", () => {
+      let lastOperatorIndex = displayValue.search(/[-+*/]/g);
+      if (lastOperatorIndex >= 0 && !displayValue.slice(lastOperatorIndex + 1).includes(".")) {
+          displayValue += ".";
+      } else if (lastOperatorIndex < 0 && !displayValue.includes(".")) {
+          displayValue += ".";
+      }
+      document.getElementById("display").textContent = displayValue;
+  });
+});
+
+document.querySelectorAll(".operator").forEach(button => {
+  button.addEventListener("click", () => {
+      if (displayValue !== "" && !isNaN(displayValue.slice(-1))) {
+          displayValue += ` ${button.value} `;
+      }
+      document.getElementById("display").textContent = displayValue;
+  });
+});
+
+document.querySelector(".clear").addEventListener("click", () => {
+  displayValue = "";
+  document.getElementById("display").textContent = displayValue;
+});
+
+document.querySelector(".delete").addEventListener("click", () => {
+  if (displayValue !== "") {
+      displayValue = displayValue.slice(0, -1);
+      document.getElementById("display").textContent = displayValue;
+  }
+});
+
+document.querySelector(".equals").addEventListener("click", () => {
+  let parts = displayValue.match(/^\s*(-?\d*\.?\d+)\s*([-+*/])\s*(-?\d*\.?\d+)\s*$/);
+  if (parts) {
+      let num1 = parseFloat(parts[1]);
+      let operator1 = parts[2];
+      let num2 = parseFloat(parts[3]);
+      let result1 = operate(num1, num2, operator1); /*add-on*/
+      displayValue = result1.toString();
+      document.getElementById("display").textContent = displayValue;
+  }
 });
